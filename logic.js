e6b62e848f66e5261f967f898d17e87da43c9875
Vedant <vedantpatle594@gmail.com>
2024-02-23 20:00:07 +0530

    first commit

---
+++ logic.js
@@ -0,0 +1,18 @@
+const bars = document.getElementById("open");
+const menu = document.getElementById("list-container");
+const close = document.getElementById("close");
+
+window.onresize = function () {
+  if (window.innerWidth > 600) {
+    menu.style.width = "70%";
+  } else {
+    menu.style.width = "0%";
+  }
+};
+bars.addEventListener("click", () => {
+  menu.style.width = "100%";
+});
+
+close.addEventListener("click", () => {
+  menu.style.width = "0%";
+});
